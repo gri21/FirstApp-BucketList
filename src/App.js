1144e31f00ca2f307c474e04df97e065b0fa4d86
Gemma Isom <gemma.isom@gmail.com>
2021-07-04 21:38:01 +0100

    styling and initial set up

--- src/App.js
+++ src/App.js
@@ -1,30 +1,118 @@
-import logo from './cat3.png';
+import logo from './Sun2.png';
 import './App.css';
+import React, { useState } from 'react';
+import { validCountries } from './validCountries';
 
 function App() {
+  const [country, setCountry] = useState('');
+  const [countryTemp, setCountryTemp] = useState('');
+  const [countryList, setCountryList] = useState(["Sri Lanka", "UK", "France"]); //populated for testing purposes. Should be []
+  const [YouAR, setar] = useState('');
+  const handleCountryDelete = countryToRemove => {
+    let countryListRemoved = [];
+    for (let i = 0; i < countryList.length; i++) {
+      if (countryList[i] !== countryToRemove) {
+        countryListRemoved.push(countryList[i]);
+        // debugger;
+      };
+    };
+    setCountryList(countryListRemoved);
+    setCountry(countryToRemove);
+    setar('You removed');
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>
           Bucket List - Interactive
         </h1>
+        <img src={logo} className="App-logo" alt="logo" />
         <h2>
-        <code>Built in React</code>
+          <code>Built in React</code>
         </h2>
       </header>
-      <div className="App-content">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my <code>first app</code> in React.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.tripadvisor.co.uk/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Trip Advisor
-        </a>
+      <div className="background-first">
+        <div className="App-content">
+          <div className="Welcome">
+            <p>
+              Welcome to my first app in React.
+            </p>
+            <div className="same-row">
+              <input className="text-box"
+                type="search"
+                name="dest"
+                placeholder="Enter Next Bucket-List Destination"
+                list="validCountries"
+                onChange={event => setCountryTemp(event.target.value)}
+              />
+              <datalist id="validCountries">
+                {validCountries.map(x => <option value={x} />)}
+              </datalist>
+              <button onClick={() => {
+                if (validCountries.includes(countryTemp)) {
+                  setar('You added');
+                  setCountry(countryTemp);
+                  setCountryList([...new Set([...countryList, countryTemp])]);
+                };
+              }
+              }>
+                Add Country
+              </button>
+            </div>
+            <p>
+              No Inspiration? Have a look at the links below:
+            </p>
+            <a
+              className="App-link"
+              href="https://www.tripadvisor.co.uk/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Trip Advisor
+            </a>
+            <a
+              className="App-link"
+              href="https://www.lonelyplanet.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Lonely Planet
+            </a>
+            <a
+              className="App-link"
+              href="https://www.gadventures.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              G Adventures
+            </a>
+            <p>{YouAR} {country}</p>
+          </div>
+        </div>
+      </div>
+      <div className="background-second"> {/*className needs to have a captial N!!*/}
+        <div className="App-content2">
+          <p>{YouAR} {country}</p>
+          {countryList.map(x => <div className="Dynamic-Sections">
+            <div>{x}</div>
+            <div
+              className="delete"
+              type="button"
+              onClick={() => handleCountryDelete(x)}
+            >
+              X
+            </div>
+            <input className="text-box"
+              type="text"
+              placeholder="Notes"
+            />
+            <input className="text-box"
+              type="text"
+              placeholder="Useful links"
+            />
+          </div>
+          )}
+        </div>
       </div>
     </div>
   );
